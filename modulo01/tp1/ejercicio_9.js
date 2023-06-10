fd0791010e9dd1690df26af62888570b6d43b0e7
NatashaFalicoff <122556520+NatashaFalicoff@users.noreply.github.com>
2023-06-10 12:44:48 -0300

    Create ejercicio_9.js

---
+++ modulo01/tp1/ejercicio_9.js
@@ -0,0 +1,13 @@
+let vocales = ["A", "E", "I", "O", "U"];
+
+document.write(`las vocales son: <br>`);
+
+document.write(`${vocales[0]} <br>`);
+
+document.write(`${vocales[1]} <br>`);
+
+document.write(`${vocales[2]} <br>`);
+
+document.write(`${vocales[3]} <br>`);
+
+document.write(`${vocales[4]} <br>`);
